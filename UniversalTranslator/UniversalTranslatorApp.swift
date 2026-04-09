98f84d5c6a7382002fb87f399cb491cf1679a8bc
ZachNagengast <znagengast@gmail.com>
2026-04-08 19:29:11 -0700

    Initial Commit

---
+++ UniversalTranslator/UniversalTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalTranslatorApp.swift
+//  UniversalTranslator
+//
+//  Created by Zach Nagengast on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct UniversalTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniversalTranslatorTests/UniversalTranslatorTests.swift
@@ -0,0 +1,18 @@
+//
+//  UniversalTranslatorTests.swift
+//  UniversalTranslatorTests
+//
+//  Created by Zach Nagengast on 4/8/26.
+//
+
+import Testing
+
+struct UniversalTranslatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
